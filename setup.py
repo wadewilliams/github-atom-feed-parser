bae4d952e1d7f1f9890ed8bc52ee8d2c5b227535
Wade Williams <wade@wadewilliams.com>
2015-03-21 12:58:40 -0700

    Setup tweaks

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name = 'github_feedparser',
     packages = ['github_feedparser'],
+    install_requires = ['feedparser==5.1.3'],
     version = '0.1',
     license = 'MIT',
     description = 'Python module to parse a GitHub repository\'s Atom Feed',
@@ -11,5 +12,4 @@ setup(
     url = 'https://github.com/wadewilliams/github-atom-feed-parser',
     download_url = 'https://github.com/wadewilliams/github-atom-feed-parser/tarball/0.1',
     keywords = ['github', 'rss', 'atom', 'feedparser'],
-    install_requires = ['feedparser']
 )
